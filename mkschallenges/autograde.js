c3fd0afeb2b7d1984a41da5d557cd9b3be388f6c
JMGoldsmith <spartanaudio@gmail.com>
2015-04-22 12:52:56 -0700

    autograde update

--- mkschallenges/autograde.js
+++ mkschallenges/autograde.js
@@ -1,23 +1,24 @@
 function autograde (report) {
-    var arr = report.split("\n")
+    var arr = report.split("\n");
     for (var i = 0; i< arr.length; i++){
-        var student = arr[i].split(' ')
-        var grade_array = []
+        var student = arr[i].split(' ');
+        var grade_array = [];
         for (var j = 1; j < student.length; j++){
-            grade_array.push(parseInt(student[j]))
+            grade_array.push(parseInt(student[j]));
         }
-        var avg_array = []
-        var total = 0
+        var avg_array = [];
+        var total = 0;
         for (var k = 0; k<grade_array.length; k++){
-            total += grade_array[i]
+            total += grade_array[k];
         }
-        avg_array.push(total)
-        var avg = avg_array/grade_array.length
-        console.log(avg)
+        avg_array.push(total);
+        var avg = avg_array/grade_array.length;
+        console.log(avg);
+        //push to object here
     }
 }
 
 
 
-var report = "Jon 19 14 15 15 16\nJeremy 15 11 10 15 16\nJesse 19 17 20 19 18"
-autograde(report)
+var report = "Jon 19 14 15 15 16\nJeremy 15 11 10 15 16\nJesse 19 17 20 19 18";
+autograde(report);
